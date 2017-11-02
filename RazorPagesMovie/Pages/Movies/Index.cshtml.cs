1c920c3eb041900906de8db916afc9800845cc54
Enrico Papi <enrico.papi@gmail.com>
2017-11-02 17:36:43 +0100

    add search by title

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -20,9 +20,23 @@ namespace RazorPagesMovie.Pages.Movies
 
         public IList<Movie> Movie { get;set; }
 
+        /*
         public async Task OnGetAsync()
         {
             Movie = await _context.Movie.ToListAsync();
+        }
+        */
+        public async Task OnGetAsync(string searchString)
+        {
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            Movie = await movies.ToListAsync();
         }
     }
 }
