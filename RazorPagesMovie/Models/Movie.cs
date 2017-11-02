86b03746837b5b5f49505312c4ef552d4637f225
Enrico Papi <enrico.papi@gmail.com>
2017-11-02 17:45:02 +0100

    add rating model field

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -16,5 +16,7 @@ namespace RazorPagesMovie.Models
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
+
+        public string Rating { get; set; }
     }
 }
